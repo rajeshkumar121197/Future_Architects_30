b99fe52b53fc10cd5a90686bc1723501e62c1dff
Shivam140728 <shivam.kumar140728@gmail.com>
2024-11-16 18:10:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
-import Preprocessor
-# import matplotlib.pyplot as plt
-# import pydeck as pdk
-# import seaborn as sns
+import Preprocessor as Pre
+
+import pydeck as pdk
+import seaborn as sns
 
 # changing the layout of streamlit
 st.set_page_config(layout = "wide")
+
+import warnings
+warnings.filterwarnings('ignore') #suppresses the warnings displayed by the code editor.
+
 
 # Load the dataset
 df = pd.read_csv("Detailed Register and Unregistered cases (Sexual assault) (Punished Release) 2018.csv")
@@ -23,16 +28,15 @@ st.sidebar.header("Filter by ")
 st.sidebar.multiselect("State Filter",df["State/UT"])
 
 
-# # Plotting the data
-# plt.figure(figsize=(10, 5))
-# sns.barplot(x="State/UT", y="Rape (Total) (Sec.376 IPC)")
-# plt.xlabel("State/UT")
-# plt.ylabel("Rape (Total) (Sec.376 IPC)")
-# plt.title("Total Rape Cases by State/UT (Filtered)")
+# Plotting the data
+plt.figure(figsize=(10, 5))
+sns.barplot(x="State/UT", y="Rape (Total) (Sec.376 IPC)")
+plt.xlabel("State/UT")
+plt.ylabel("Rape (Total) (Sec.376 IPC)")
+plt.title("Total Rape Cases by State/UT (Filtered)")
 
-# # Display the plot
-# st.pyplot(plt)
-
+# Display the plot
+st.pyplot(plt)
 
 
 
